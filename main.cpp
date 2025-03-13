55587941bee467e88c348e4f6762251036a2cdb4
Thiago Fabri <nysevnine@gmail.com>
2025-03-13 13:41:59 -0300

    Primeiro commit - adicionando arquivos do projeto

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>     
+#include<locale.h>     
+using namespace std;
+int main (){
+    setlocale(LC_ALL, "Portuguese ");   
+    string like;                        
+    cout<<"deixa o like e se inscreva ai mano "<<endl;
+    cin>>like;
+    cout<<"Obrigado!"<<endl;
+
+    return 0;
+}
